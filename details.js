2e272bed53ea37c8b5838341cf96522e91f4ff07
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2023-04-09 20:37:29 +0200

    Add files via upload

--- details.js
+++ details.js
@@ -1,23 +1,16 @@
 import { baseUrl } from "./index.js";
 
 const detailsContainer = document.querySelector(".details__container");
-const punchline = document.querySelector(".punchline");
+const punchline = document.querySelector(".details__punchline");
 punchline.style.display = "none";
-
-
-
 
 function getQueryParam() {
   const currentURL = document.location.href;
 
-
   let params = new URL(currentURL).searchParams;
-
   const getQueryParam = "/" + params.get("id");
-
   console.log(getQueryParam);
-
-  detailsContainer.innerHTML = `<div class="detail__container loading">Loading...</div>`;
+  detailsContainer.innerHTML = `<div class="details__container loading"><p>Loading...</p></div>`;
 
   async function fetchById() {
     const response = await fetch(baseUrl + getQueryParam);
@@ -27,24 +20,24 @@ function getQueryParam() {
     detailsContainer.innerHTML = ``;
 
     detailsContainer.innerHTML += `
-    <div class="detail__container">
-    <div>type: ${jsonData.type}
-     <div>setup: ${jsonData.setup}
+    <div class="details__container">
+    <div><p>type: ${jsonData.type}</p>
+     <div><p>setup: ${jsonData.setup}</p>
      </div>
-     <div>Punchline:</div>
+     <div><p>Punchline:<p/></div>
      <button class="revealPunchline">Reveal</button>
+   <div>`;
 
-    <div>`;
-
-    punchline.innerHTML = `  
- <div class="detail__container">${jsonData.punchline}
+  punchline.innerHTML = `  
+ <div class="details__container">
+     <p> ${jsonData.punchline} </p>
  </div>
-     `;
+    `;
 
     console.log(jsonData.punchline);
 
     const revealButton = document.querySelector(".revealPunchline");
-    revealButton.style.width = "100px"
+    revealButton.style.width = "100px";
     function revealPunchline() {
       const currentDisplayValue = punchline.style.display;
       console.log(currentDisplayValue);
@@ -52,13 +45,11 @@ function getQueryParam() {
       if (punchline) {
         punchline.style.display = "block";
         console.log(punchline.style);
-        revealButton.textContent="Hide";
-       
+        revealButton.textContent = "Hide";
       }
       if (currentDisplayValue == "block") {
         punchline.style.display = "none";
-        revealButton.textContent = "Reveal"
-        
+        revealButton.textContent = "Reveal";
       }
     }
 
--- index.js
+++ index.js
@@ -1,62 +1,37 @@
-
-
 export const baseUrl = "https://api.noroff.dev/api/v1/jokes";
-const container = document.querySelector(".container");
-const jokeType = document.querySelector("#jokeType");
+const container = document.querySelector(".jokes__container");
+const jokeType = document.querySelector("#jokes__type");
 
 async function fetchAllData() {
+  const currentJokeType = jokeType.options[jokeType.selectedIndex].text;
+  const jokeTypeContainer = document.querySelectorAll(".jokes__select");
 
+  container.innerHTML= `<div class="jokes__loading">...Loading</div>`;
+  for (let i = 0; i < 3; i++) {
+    jokeTypeContainer[i].addEventListener("click", function () {
+      location.reload();
+    });
+  }
 
-const currentJokeType = jokeType.options[jokeType.selectedIndex].text;
-const jokeTypeContainer = document.querySelectorAll(".jokeType__select");
-
-for(let i=0; i < 3; i++) {
-
-
-
-jokeTypeContainer[i].addEventListener("click", function() {
-
-
-
-location.reload();
-
-})};
 
   try {
+
     const response = await fetch(baseUrl);
     const json = await response.json();
 
 
+    var dataValue = json;
 
+    container.innerHTML= ``;
 
-
-    json.sort((a, b) => a.setup.localeCompare(b.setup)) //filter by programming or general
- 
-
-
-
-
-var dataValue = json;
-
-
-
-  if(currentJokeType=="Programmer") {
-
-  
-
-
-  var dataValue = json.filter(element => element.type=="programming")
-  console.log(dataValue)
-
-  } else if(currentJokeType == "General") {
-    var dataValue = json.filter(element => element.type=="general")
-
-  }
-    dataValue.forEach((item) => 
-  {
-
-    
-
+    if (currentJokeType == "Programmer") {
+      var dataValue = json.filter((element) => element.type == "programming");
+      console.log(dataValue);
+    } else if (currentJokeType == "General") {
+        var dataValue = json.filter((element) => element.type == "general");
+    }
+    dataValue.forEach((item) => {
+   
       container.innerHTML += `
       <li>
       <a href= "details.html?id=${item.id}"> 
@@ -68,7 +43,7 @@ var dataValue = json;
 
       </a>     </li>
       `;
-  });
+    });
   } catch (e) {
     console.error(e);
   } finally {
@@ -76,5 +51,5 @@ var dataValue = json;
   }
 }
 
-fetchAllData();
 
+fetchAllData();
